56b68359ea4083a1b26590ea8dd2fa45f2cf93d3
Silje Fjørtoft <Silje Fjrtoft>
2016-04-29 12:41:13 +0200

    Change index to load iframes when pressing the various buttons in the sidebar

--- scripts/index.js
+++ scripts/index.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
     $("p").click(function(){
         $(this).hide();
     });
+	$("#intro").click(function(){
+		$("#pages").attr('src', 'pages/intro.html'); 
+	});
+	$("#minispill").click(function(){
+		$("#pages").attr('src', 'pages/minispill.html'); 
+	});
 }); 
